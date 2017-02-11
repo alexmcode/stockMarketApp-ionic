9294173def2a095a096d6da6323fddd01d504c2d
alex <musca.alexandru@gmail.com>
2017-02-11 09:13:20 +0200

    Bug fixes with active class for button and API calls to yahoo finance

--- www/js/services.js
+++ www/js/services.js
@@ -3,13 +3,15 @@ angular.module('firstApp.services', [])
 // Function that has the purpose to generate an object/function that will represent
 // this service - stockDataService - to the entire application. This object/function
 // will then be injected into outer components, functions, services etc.
-.factory('stockDataService', function($q, $http) {
+.factory('stockDataService', function($q, $http, encodeURIService) {
 // $q - this service is an implementation of a promise. It help executing async code;
 // $http - service for doing requests;
 
   var getDetailsData = function(ticker) {
     var deferred = $q.defer(),
-    url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20IN%20(%22" + ticker + "%22)&format=json&env=http://datatables.org/alltables.env";
+    url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + ticker + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+    // query = 'select * from yahoo.finance.quotes where symbol IN ("' + ticker + '")',
+    // url = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIService.encode(query) + '&format=json&env=http://datatables.org/alltables.env';
 
     $http.get(url)
       .success(function(json) {
@@ -50,4 +52,12 @@ angular.module('firstApp.services', [])
   };
   // The service will return a promise, that will return a valid/not valid result
 
+})
+
+.factory('encodeURIService', function() {
+  return {
+    encode: function(string) {
+      return encodeURIComponent(string).replace(/\"/g, "%22").replace(/\ /g, "%20").replace(/[!'()]/g, escape);
+    }
+  };
 });
