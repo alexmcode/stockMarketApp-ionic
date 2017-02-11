0bcaa255aaf7875d3db0ef43f686524b30fc7e7c
alex <musca.alexandru@gmail.com>
2017-02-11 09:40:04 +0200

    Added URL encoding

--- www/js/services.js
+++ www/js/services.js
@@ -1,17 +1,31 @@
 angular.module('firstApp.services', [])
 
-// Function that has the purpose to generate an object/function that will represent
+// .factory(): Function that has the purpose to generate an object/function that will represent
 // this service - stockDataService - to the entire application. This object/function
 // will then be injected into outer components, functions, services etc.
+.factory('encodeURIService', function() {
+  return {
+    encode: function(string) {
+      console.log(string);
+      return encodeURIComponent(string).replace(/\"/g, "%22").replace(/\ /g, "%20").replace(/[!'()]/g, escape);
+    }
+  };
+})
+
 .factory('stockDataService', function($q, $http, encodeURIService) {
 // $q - this service is an implementation of a promise. It help executing async code;
 // $http - service for doing requests;
 
   var getDetailsData = function(ticker) {
     var deferred = $q.defer(),
-    url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + ticker + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
-    // query = 'select * from yahoo.finance.quotes where symbol IN ("' + ticker + '")',
-    // url = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIService.encode(query) + '&format=json&env=http://datatables.org/alltables.env';
+    // Non-encoded URL:
+    // url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + ticker + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+
+    // Encoded URL: good for cases when working with long and complex URLs
+    query = 'select * from yahoo.finance.quotes where symbol IN ("' + ticker + '")',
+    url = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIService.encode(query) + '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+
+    console.log(url);
 
     $http.get(url)
       .success(function(json) {
@@ -52,12 +66,4 @@ angular.module('firstApp.services', [])
   };
   // The service will return a promise, that will return a valid/not valid result
 
-})
-
-.factory('encodeURIService', function() {
-  return {
-    encode: function(string) {
-      return encodeURIComponent(string).replace(/\"/g, "%22").replace(/\ /g, "%20").replace(/[!'()]/g, escape);
-    }
-  };
 });
